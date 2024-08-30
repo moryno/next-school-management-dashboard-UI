6cb27b81a57a972d6af189ecc918d518c1439269
moryno <mauricenganga41@gmail.com>
2024-08-30 13:08:59 +0300

    Add sidemenu menus

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Menu = () => {
+  return (
+    <article className='mt-4 text-xs'>
+      {menuItems.map((menu) => (
+        <div className="flex flex-col gap-2" key={menu.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            { menu.title }
+          </span>
+          { menu.items.map((item) => (
+            <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+              <Image src={item.icon} alt="this is menu icon" width={20} height={20} />
+              <span className="hidden lg:block">{ item.label }</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </article>
+  )
+}
+
+export default Menu
 const menuItems = [
   {
     title: "MENU",
