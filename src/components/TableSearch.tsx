7e4ff2fc0202dc82ce3f068bfcdb6f381d199855
moryno <mauricenganga41@gmail.com>
2024-08-31 09:08:08 +0300

    Add table search component

---
+++ src/components/TableSearch.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+const TableSearch = () => {
+  return (
+    <div className="flex w-full md:w-auto items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+        <Image src="/search.png" alt="this is a search icon" width={14} height={14} />
+        <input type="text" placeholder="Search..." className="outline-none w-[200px] p-2 bg-transparent" />
+    </div>
+  )
+}
+
+export default TableSearch
