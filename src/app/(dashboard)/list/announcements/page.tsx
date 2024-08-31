0de5d4da1436ec76d63f9e405efc297cb8368a44
moryno <mauricenganga41@gmail.com>
2024-08-31 13:05:12 +0300

    Add announcement list

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,27 +1,23 @@
 import GridComponent from "@/components/GridComponent"
 import Pagination from "@/components/Pagination"
 import TableSearch from "@/components/TableSearch"
-import { eventsData, role } from "@/lib/data"
+import { announcementsData, role } from "@/lib/data"
 import Image from "next/image"
 import Link from "next/link"
 import { useCallback } from "react"
-export type EventType = {
+export type AnnouncementType = {
     id: number;
     title: string;
     class: string;
     date: string;
-    startTime: string;
-    endTime: string;
 }
-const EventListPage = () => {
+const AnnouncementListPage = () => {
 
-    const renderRow = useCallback(( record: EventType) => (
+    const renderRow = useCallback(( record: AnnouncementType) => (
         <tr key={record.id} className="border-b border-gray100 even:bg-slate-50 text-[13px] hover:bg-purpleLight">
          <td className="px-3 py-4 font-semibold">{ record.title }</td>
          <td className="w-[10%] text-center">{ record.class }</td>
          <td className="hidden md:table-cell text-center">{ record.date }</td>
-         <td className="hidden md:table-cell  text-center">{ record.startTime }</td>
-         <td className="hidden md:table-cell text-center">{ record.endTime }</td>
          <td>
             <div className="flex items-center gap-2">
                 <Link href={`/list/teachers/${record.id}`}>
@@ -44,7 +40,7 @@ const EventListPage = () => {
   return (
     <section className="bg-white p-4 rounded-md shadow-md flex-1 m-4 mt-0">
         <article className="flex items-center justify-between">
-            <h1 className="hidden md:block text-sm font-semibold">Events</h1>
+            <h1 className="hidden md:block text-sm font-semibold">Announcements</h1>
             <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
                 <TableSearch />
                 <div className="flex items-center gap-4 self-end">
@@ -65,17 +61,17 @@ const EventListPage = () => {
                 </div>
             </div>
         </article>
-         <GridComponent columns={columns} renderRow={renderRow} datasource={eventsData} />
+         <GridComponent columns={columns} renderRow={renderRow} datasource={announcementsData} />
          <Pagination />
     </section>
   )
 }
 
-export default EventListPage
+export default AnnouncementListPage
 
 const columns = [
     {
-        header: "Event",
+        header: "Title",
         accessor: "title",
         className: "px-3"
     },
@@ -85,18 +81,8 @@ const columns = [
          className: "text-center w-[10%]"
     },
     {
-        header: "Date",
+        header: "Announcement Date",
         accessor: "date",
-        className: "hidden md:table-cell text-center"
-    },
-    {
-        header: "Start Time",
-        accessor: "startTime",
-        className: "hidden md:table-cell text-center"
-    },
-    {
-        header: "End Time",
-        accessor: "endTime",
         className: "hidden md:table-cell text-center"
     },
     {
